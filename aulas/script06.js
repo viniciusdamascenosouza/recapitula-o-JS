9e9fa8aafb9527741671721c4e76c89b37ea7ea6
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-16 18:16:01 -0300

    aula 08

--- aulas/script06.js
+++ aulas/script06.js
@@ -8,3 +8,19 @@ var resultadoDaSoma = window.alert('As soma dos números inseridos é: ' + soma 
 var nome = window.prompt('Qual é o seu nome?')
 
 document.write(`Seja bem-vindo, ${nome}!`)
+
+console.log((5 + 3) / 2)
+
+var a = 5 + 3 /* = 8 */
+var b = a % 5 /* = 3 */
+var c = 5 * b ** 2 /* = 45 */
+var d = 10 - a / 2 /* = 6 */
+var e = 6 * 2 / d /* = 2 */
+var f = b % e + 4 / e /* =  */
+
+const somaDoResultado = a + b + c + d + e + f
+console.log(somaDoResultado)
+
+var n = 10
+n = 10 * 2
+console.log(n)
